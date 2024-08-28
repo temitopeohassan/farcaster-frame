b62dacf460a31da6773929db8e7ccf5369b36240
temitopeohassan <temitope.hassan.2@gmail.com>
2024-08-28 17:01:57 +0100

    type error correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-"use client"
+"use client";
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const fetchCommunityData = async () => {
+// Define the Community interface
+interface Community {
+  id: string;
+  name: string;
+  description: string;
+  // Add any other fields you expect from the API
+}
+
+const fetchCommunityData = async (): Promise<Community[]> => {
   try {
     const response = await axios.get('https://api.rounds.wtf/communities');
     return response.data;
@@ -13,7 +21,7 @@ const fetchCommunityData = async () => {
 };
 
 const CommunityList = () => {
-  const [communities, setCommunities] = useState([]);
+  const [communities, setCommunities] = useState<Community[]>([]);
 
   useEffect(() => {
     fetchCommunityData().then(setCommunities);
@@ -21,8 +29,8 @@ const CommunityList = () => {
 
   return (
     <div>
-      {communities.map((community, index) => (
-        <div key={index}>
+      {communities.map((community) => (
+        <div key={community.id}>
           <h3>{community.name}</h3>
           <p>{community.description}</p>
         </div>
